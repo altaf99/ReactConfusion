11ed0b27eeb11c033787a5aa41fda776310b8b1f
altaf99 <iamaltafshaikh07@gmail.com>
2020-07-10 10:15:37 +0530

    React Router-- added soe routes for home and menu

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 
 import Menu from './MenuComponent';
@@ -7,6 +7,7 @@ import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
 
 class Main extends Component {
 
@@ -23,12 +24,22 @@ class Main extends Component {
   }
 
   render() {
+
+
+    const HomePage = () => {
+      return(
+          <Home 
+          />
+      );
+    }
+      
     return (
       <div>
-         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-        <Footer />
+         <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Redirect to="/home" />
+          </Switch>
       </div>
     );
   }
