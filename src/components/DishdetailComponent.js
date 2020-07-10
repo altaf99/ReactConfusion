81b924e942911e5071a1058f1b458af4b628e83f
altaf99 <iamaltafshaikh07@gmail.com>
2020-07-10 10:48:41 +0530

    Single Page Applications Part 2-added common header footer and passing id in url ans grabbing id in routes

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
  import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
+ import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
         if (dish != null)
@@ -21,49 +22,59 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
     function RenderComments({comments}) {
         
-        if (comments!= null)
+        if (comments!= null){
+            comments = comments.map((comment) => {
+                return(
+                
+                    <ul key={comment.id} className="list-unstyled">
+                    <li>{comment.comment}</li>
+                        <li>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
+                    </ul>
+                
+                );
+            });
             return(
                <div className="container">
                     <h4>Comments</h4>
                     {comments}
                </div>
             );
-                        
-        else
+              
+        }
+        else{
             return(
                 <div></div>
             );
+
+        }
+            
     }
 
     const  DishDetail = (props) => {
 
-            const dish = props.dish;
-            var comments = null;
-            if(dish){
-                comments = dish.comments.map((comment) => {
-                    return(
-                      
-                            <ul key={comment.id} className="list-unstyled">
-                            <li>{comment.comment}</li>
-                                <li>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
-                            </ul>
-                       
-                    );
-                 });
-            }
-        
-        return(
-            <div className="container">
+            return (
+                <div className="container">
                 <div className="row">
-                    <div  className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} ></RenderDish>
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
                     </div>
-                    <div  className="col-12 col-md-5 text-left m-1">
-                        <RenderComments comments={comments}></RenderComments>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
-            </div>
-        );
+                </div>
+            );
        
     }
 
