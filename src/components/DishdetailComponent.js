112a720e00fbd0e06132cfb20ff18a985e0a5895
altaf99 <iamaltafshaikh07@gmail.com>
2020-07-24 21:08:14 +0530

    Fetch from Server.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,6 +7,8 @@ import { Button, Modal, ModalHeader, ModalBody, Row, Col, Label } from 'reactstr
 
 import { Control,LocalForm, Form, Errors, actions } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -17,7 +19,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
         if (dish != null)
             return(
                 <Card>
-                    <CardImg width="100%" top src={dish.image} alt={dish.name} />
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                       <CardTitle>{dish.name}</CardTitle>
                       <CardText>{dish.description}</CardText>
